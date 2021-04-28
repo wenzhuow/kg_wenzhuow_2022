6e56d9d1c530434ed6b40fd6afac6c81caa38001
wenzhuow <wenzhuow@uci.edu>
2021-04-27 17:51:47 -0700

    feat: read input and print in console

--- main.js
+++ main.js
@@ -6,3 +6,23 @@ function translate(int_string){
 	}
 	return translated_string
 }
+
+function readarg(){
+	var int_array = process.argv.slice(2);
+	var phonetic_array = [];
+	for (var i = 0; i < int_array.length; i++) {
+		phonetic_array.push(translate(int_array[i]));
+	}
+	return phonetic_array;
+}
+
+function output(content){
+	text = "";
+	for (var i = 0; i < content.length; i++) {
+		text+=content[i]+","
+	}
+	console.log( text.slice(0,-1) )
+}
+
+var ans = readarg();
+output(ans);
